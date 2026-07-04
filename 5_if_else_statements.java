d2795fbc68009833e27f3f9a5bf64b81eeb4d4ed
Phal Developer <sophalsam04@gmail.com>
2026-07-04 10:43:15 +0700

    Create 5_if_else_statements.java

---
+++ 5_if_else_statements.java
@@ -0,0 +1,27 @@
+public  class Main {
+    public static void main(String[] args) {
+            // if else statements
+        int age = 22;
+            if(age>=18 && age<=22){
+                    System.out.println("You can vote the eligible...!");
+            }else{
+                System.out.println("You cannot vote the eligible...!");
+            }
+
+
+                int number = 7;
+            if(number % 2== 0){
+                    System.out.println("Event Number");
+            }else{
+                System.out.println("Odd Number");
+            }
+
+            int num = 20;
+            if(num % 2 == 0){
+                    System.out.println("Event number");
+            }else{
+                System.out.println("Odd number");
+            }
+    }
+
+}
