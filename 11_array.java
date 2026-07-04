11d63839ef7f1132fd414b5689ed66bc77b86d87
Phal Developer <sophalsam04@gmail.com>
2026-07-04 11:47:21 +0700

    Create 11_array.java

---
+++ 11_array.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // Array in Java
+        int[] array = {1,2,3,4,5};
+
+            for (int number : array){
+                    System.out.println("Array of Java : "+number);
+            }
+            
+            String[] fruits = {"Orange", "Apple","Banana","Cherry","Mango","Lettuce"};
+                for (String fruit : fruits){
+                        System.out.println(fruit);
+                }
+
+                double[] value = {1.3,54.3,5.4};
+
+                for (double v : value){
+                        System.out.println(v);
+                }
+    }
+
+
+}
