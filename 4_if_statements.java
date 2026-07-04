538be7a209ff80aba93f03c4c548234701ac7dba
Phal Developer <sophalsam04@gmail.com>
2026-07-04 10:34:14 +0700

    Create 4_if_statements.java

---
+++ 4_if_statements.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+                // Conditional Statement in Java Programing
+
+            // if statements
+        int age = 18;
+            if(age>=18){
+                System.out.println("You can to vote the eligible...!");
+            }
+
+            double salary = 300;
+                if(salary >=200){
+                        System.out.println("You have salary of hard working...!");
+                }
+
+
+             int bonus = 200;
+                if(bonus < salary){
+                        System.out.println("Bonus is greater than salary...!");
+                }
+
+    }
+
+
+}
