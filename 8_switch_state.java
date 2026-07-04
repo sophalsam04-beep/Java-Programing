3a622729821c2d3406b308644a37a91bf0b6d671
Phal Developer <sophalsam04@gmail.com>
2026-07-04 11:10:41 +0700

    Create 8_switch_state.java

---
+++ 8_switch_state.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+            // switch statements
+        int day = 3;
+
+        switch (day){
+            case 1:
+                System.out.println("Monday");
+                break;
+
+            case 2:
+                System.out.println("Tuesday");
+                break;
+
+            case 3:
+                System.out.println("Wednesday");
+                break;
+
+            case 4:
+                System.out.println("Thursday");
+                break;
+
+            case 5:
+                System.out.println("Friday");
+                break;
+
+            case 6:
+                System.out.println("Saturday");
+                break;
+
+            case 7:
+                System.out.println("Sunday");
+                break;
+
+            default:
+                System.out.println("Invaid Email and password...!");
+        }
+    }
+
+
+}
