05e965e5723abbad3011708c2b1b5f41e89e1343
Phal Developer <sophalsam04@gmail.com>
2026-07-04 10:58:11 +0700

    Create 7_Nested_statements.java

---
+++ 7_Nested_statements.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+            // nested Loop statements
+        int age = 30;
+        boolean isStudents = true;
+
+            if( age>= 20 ){
+                if(isStudents){
+                   System.out.println("You can driver...!");
+                } else {
+                    System.out.println("You have to Driver but you need to have license...!");
+                }
+            }else{
+                System.out.println("Young boy and girl can driver too...");
+            }
+    }
+
+
+
+}
