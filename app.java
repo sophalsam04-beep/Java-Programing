b3ec208889be343b5e6459c6762a343e1a3dfd08
Phal Developer <sophalsam04@gmail.com>
2026-07-04 09:18:29 +0700

    Add Main class with basic variable declarations

---
+++ app.java
@@ -0,0 +1,26 @@
+public  class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+
+                    // Variable
+        int id = 101;
+        String name = "Un virak";
+        String role = "Java Developer";
+        int age = 42;
+        boolean isTeacher = true;
+        char grade = 'A';
+        double price = 40.4;
+        float salary = 4000f;
+        long riel = 5000000L;
+
+        System.out.println("ID : "+id);
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("Role : "+role);
+        System.out.println("Is Students :"+isTeacher);
+    }
+
+
+
+}
