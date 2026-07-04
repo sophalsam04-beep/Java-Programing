74fc9789577380c63d1701afb3fcbc571ae5ce40
Phal Developer <sophalsam04@gmail.com>
2026-07-04 11:57:19 +0700

    Create 12_String.java

---
+++ 12_String.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // String
+        String name = "Flutter Developers";
+
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.trim());
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+        System.out.println(name.substring(5));
+        System.out.println(name.substring(0,5));
+
+
+    }
+
+}
