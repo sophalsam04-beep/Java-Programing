c3605c840499a2602c0fb673f627dacb5ba3de18
Phal Developer <sophalsam04@gmail.com>
2026-07-04 14:39:58 +0700

    Create 13_Encapulation.java

---
+++ 13_Encapulation.java
@@ -0,0 +1,40 @@
+// Encapsulation In Java Programing
+class Student{
+    // Private data member
+        private int id;
+        private String name;
+        private String role;
+
+        // Methods setter and getter
+            public void setId(int id){
+                    this.id=id;
+            }
+            public int getId(){
+                return id;
+            }
+
+            public void setName(){
+                this.name = name;
+            }
+
+            public String getName(){
+                    return name;
+            }
+
+            public void setRole(String role){
+                thi.role = role;
+            }
+
+            public String getRole(){
+                    return role;
+            }
+}
+
+public class Main {
+    public static void main(String[] args) {
+            // call into object
+
+    }
+
+
+}
