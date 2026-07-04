0f9cfe6d829f2c0f5b279f8118377de342f7eb35
Phal Developer <sophalsam04@gmail.com>
2026-07-04 10:48:06 +0700

    Create 6_if_else_if_statement.java

---
+++ 6_if_else_if_statement.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // if else if statements
+        int score = 92;
+
+        if (score >= 90 && score <= 100) {
+            System.out.println("Grade A");
+        } else if (score >= 80 && score <= 90) {
+            System.out.println("Grade B");
+        } else if (score >= 70 && score <= 80) {
+            System.out.println("Grade C");
+        } else if (score >= 60 && score <= 70) {
+            System.out.println("Grade D");
+        } else if (score >= 50 && score <= 60) {
+            System.out.println("Grade E");
+        } else {
+            System.out.println("Grade F");
+        }
+
+
+    }
+
+}
